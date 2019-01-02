f0ab61b198e63b01bc0d2670119b08b42fdec738
Maryanne Wachter <wachter@arch.ethz.ch>
2019-01-02 09:52:16 -0800

    added django 2.0 support (not 2.1) and test files, still need to figure out jquery integration

--- likert_field/templatetags/likert_star_tools.py
+++ likert_field/templatetags/likert_star_tools.py
@@ -27,3 +27,27 @@ def render_stars(num, max_stars, star_set):
 
     return ''.join(
         star_set['star'] * int(num) + star_set['unlit'] * difference)
+
+def render_geo(num, max_geo, geo_set):
+    """
+    Geo icon renderer returns a HTML string of different icons
+
+    If num is None or a blank string, it returns the unanswered tag
+
+    Otherwise, the returned string will contain num solid geometric objects
+    followed by max_geo - num empty geometry
+
+    If num > max_geo, render max_geo solid geo
+
+    geo_set is a dictionary of strings with keys: geo, unlit, noanswer
+    """
+    if num is None or (isinstance(num, string_types) and len(num) == 0):
+        return geo_set['noanswer']
+
+    difference = int(max_geo) - int(num)
+    if difference < 0:
+        num = max_geo
+        difference = 0
+
+    return ''.join(
+        geo_set['geo'] * int(num) + geo_set['unlit'] * difference)
--- test_projects/django18/likert_test_app/views.py
+++ test_projects/django18/likert_test_app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
 
 # from .forms import SurveyForm
